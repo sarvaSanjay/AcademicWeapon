c18469b1c40411f111878f5d69f77a9a2bac1dc5
Kashish Mittal <113269381+04kash@users.noreply.github.com>
2023-11-05 10:17:56 -0500

    Update FlashCards.jsx

--- quizeme/src/components/FlashCards.jsx
+++ quizeme/src/components/FlashCards.jsx
@@ -8,7 +8,7 @@ function Flashcards(props) {
     const [isQuiz,setIsQuiz]= useState(false)
     return (
         <div className='flashcards'>
-            <a href = '/'><Button>Back To Home</Button></a>
+            <a href = '/AcademicWeapon'><Button>Back To Home</Button></a>
             <h1 className='heading'>FlashCards:</h1>
         <Carousel interval={null}>
             {props.flashcards.map(flashcard => {return (<Carousel.Item><Flashcard question ={flashcard.question} answer={flashcard.answer} /></Carousel.Item>)})};
