147be60b1056f0d3f3359c41622bc755d5d6e09d
Sarva Sanjay <sarva.sanjay@mail.utoronto.ca>
2023-11-04 22:37:18 -0400

    modules changed to CommonJS

--- quizeme/src/quiz_generator/flashcardGeneratorGPT.js
+++ quizeme/src/quiz_generator/flashcardGeneratorGPT.js
@@ -25,7 +25,7 @@ const data = `In the decades following its publication, the Chinese Room argumen
     weak AI, nor does it purport to show that no machine can think—Searle says that brains are machines, and brains
     think. The argument is directed at the view that formal computations on symbols can produce thought.`;
 
-export async function createFlashcards(pages){
+async function createFlashcards(pages){
     let quiz = []
     for(let i = 0; i < pages.length; i++){
         let page = pages[i];
